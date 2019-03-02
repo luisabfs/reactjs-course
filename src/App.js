14ccd2c9b9ae535458659d29049bb03c9f87a2ad
Luísa Barros <qlullyq@gmail.com>
2019-03-02 02:05:50 -0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import api from './services/api'
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 import Header from './components/Header';
 import Main from './pages/main';
